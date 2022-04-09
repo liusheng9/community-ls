de8cdeb7234b6c6f8a519b23bc6238d9cd01c0fb
ls <ls@126.com>
2022-04-09 18:42:25 +0800

    搭建环境，在测试类导入主配置类

---
+++ src/test/java/ls/learn/newcode/NewcodeApplicationTests.java
@@ -0,0 +1,15 @@
+package ls.learn.newcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = NewcodeApplication.class)
+@SpringBootTest
+class NewcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
